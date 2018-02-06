09389ad1d18b1a09d866246032d57aa4192406be
Laura Sommer <laurasommer@Lauras-MacBook-Pro.local>
2018-02-06 08:56:15 +0100

    layout

--- js.js
+++ js.js
@@ -1,58 +1,104 @@
- let menukort = [];
-
- document.addEventListener("DOMContentLoaded", hentJson);
-
- async function hentJson() {
-     let jsonData = await fetch("menu.json");
-     menukort = await jsonData.json();
-     visMenukort(menukort, "Menu");
-     lavFiltre();
- }
+let menukort = [];
 
 
- function visMenukort(menukort, overskrift) {
-     let temp = document.querySelector("[data-menutemplate]");
-     let dest = document.querySelector("[data-menukort]");
-     dest.innerHTML = "";
-     document.querySelector("#overskrift").textContent = overskrift;
+document.addEventListener("DOMContentLoaded", hentJson);
+
+async function hentJson() {
+    let jsonData = await fetch("menu.json");
+    menukort = await jsonData.json();
+    visMenukort(menukort, "Menu");
+    lavFiltre();
+}
 
 
-     menukort.forEach(madret => {
-         //klon template og indsæt person
-         let klon = temp.cloneNode(true).content;
-         klon.querySelector("[data-navn]").textContent = madret.navn;
-         klon.querySelector("[data-kortbeskrivelse]").textContent = madret.kortbeskrivelse;
-         klon.querySelector("[data-pris]").textContent = "Pris: " + madret.pris + " kr.";
+function visMenukort(menukort, overskrift) {
+
+    let temp = document.querySelector("[data-menutemplate]");
+    let dest = document.querySelector("[data-menukort]");
+    dest.innerHTML = "";
+    document.querySelector("#overskrift").textContent = overskrift;
+
+    menukort.forEach(madret => {
+        let klon = temp.cloneNode(true).content;
+        klon.querySelector("[data-navn]").textContent = madret.navn;
+        klon.querySelector("[data-kortbeskrivelse]").textContent = madret.kortbeskrivelse;
+        klon.querySelector("[data-pris]").textContent = "Pris: " + madret.pris + " kr.";
 
 
-         klon.querySelector("[data-billede]").setAttribute("src", "imgs/small/" + madret.billede + "-sm.jpg");
-         klon.querySelector("[data-billede]").alt = "billede af " + madret.navn;
-         dest.appendChild(klon);
-     });
- }
+        klon.querySelector("[data-billede]").setAttribute("src", "imgs/small/" + madret.billede + "-sm.jpg");
+        klon.querySelector("[data-billede]").alt = "billede af " + madret.navn;
 
- function lavFiltre() {
-     let forretter = menukort.filter(madret => madret.kategori == "forretter");
-     let hovedretter = menukort.filter(madret => madret.kategori == "hovedretter");
-     let desserter = menukort.filter(madret => madret.kategori == "desserter");
-     let drikkevarer = menukort.filter(madret => madret.kategori == "drikkevarer");
+        klon.querySelector(".food").setAttribute("data-id", madret.id);
+        klon.querySelector(".food").addEventListener("click", openModal);
 
 
-     //kald visRetter med de nye arrays
+        dest.appendChild(klon);
+    });
 
-     document.querySelector("#filter-alle").addEventListener("click", () => {
-         visMenukort(menukort, "Menu");
-     });
-     document.querySelector("#filter-forretter").addEventListener("click", () => {
-         visMenukort(forretter, "Forretter");
-     });
-     document.querySelector("#filter-hovedretter").addEventListener("click", () => {
-         visMenukort(hovedretter, "Hovedretter");
-     });
-     document.querySelector("#filter-desserter").addEventListener("click", () => {
-         visMenukort(desserter, "Desserter");
-     });
-     document.querySelector("#filter-drikkevarer").addEventListener("click", () => {
-         visMenukort(drikkevarer, "Drikkevarer");
-     });
- }
+}
+
+function openModal() {
+
+    let myId = this.getAttribute("data-id");
+    let single = menukort.find(food => {
+        //hvis vores myId = food.id så vis indhold.
+        if (myId == food.id) {
+            document.querySelector("#popup").style.visibility = "visible";
+            document.querySelector("[data-navn]").textContent = food.navn;
+            //console.log(food.id);
+            document.querySelector("[data-langbeskrivelse]").textContent = food.langbeskrivelse;
+            document.querySelector("[data-pris]").textContent = food.pris;
+            document.querySelector("[data-popupBillede]").setAttribute("src", "imgs/small/" + food.billede + "-sm.jpg");
+        }
+    })
+    console.log(myId);
+}
+
+
+let closeModal = document.querySelector(".close-content");
+let modal = document.querySelector("#popup");
+closeModal.onclick = function () {
+    modal.style.visibility = "hidden";
+};
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+function lavFiltre() {
+
+    let forretter = menukort.filter(madret => madret.kategori == "forretter");
+    let hovedretter = menukort.filter(madret => madret.kategori == "hovedretter");
+    let desserter = menukort.filter(madret => madret.kategori == "desserter");
+    let drikkevarer = menukort.filter(madret => madret.kategori == "drikkevarer");
+
+
+    //kald visRetter med de nye arrays
+
+    document.querySelector("#filter-alle").addEventListener("click", () => {
+        visMenukort(menukort, "Menu");
+    });
+
+    document.querySelector("#filter-forretter").addEventListener("click", () => {
+        visMenukort(forretter, "Forretter");
+
+    });
+
+    document.querySelector("#filter-hovedretter").addEventListener("click", () => {
+        visMenukort(hovedretter, "Hovedretter");
+
+    });
+
+    document.querySelector("#filter-desserter").addEventListener("click", () => {
+        visMenukort(desserter, "Desserter");
+
+    });
+
+    document.querySelector("#filter-drikkevarer").addEventListener("click", () => {
+        visMenukort(drikkevarer, "Drikkevarer");
+
+    });
+}
